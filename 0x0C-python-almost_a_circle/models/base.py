5e581b2f7bcdf8ad4d6fb249f70b47a5579f79e4
Madani00 <mamchartgmail.com>
2024-02-14 01:26:00 +0100

    add some changes to turtle last task

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -136,39 +136,37 @@ class Base:
     @staticmethod
     def draw(list_rectangles, list_squares):
         """Draw Rectangles and Squares using the turtle module.
-        Args:
-            list_rectangles (list): A list of Rectangle objects to draw.
-            list_squares (list): A list of Square objects to draw.
         """
-        turt = turtle.Turtle()
-        turt.screen.bgcolor("#b7312c")
-        turt.pensize(3)
-        turt.shape("turtle")
+        trt = turtle.Turtle()
 
-        turt.color("#ffffff")
+        trt.screen.bgcolor("#b7312c")
+        trt.pensize(3)
+        trt.shape("turtle")
+
+        trt.color("#green")
         for rect in list_rectangles:
-            turt.showturtle()
-            turt.up()
-            turt.goto(rect.x, rect.y)
-            turt.down()
+            trt.showturtle()
+            trt.up()
+            trt.goto(rect.x, rect.y)
+            trt.down()
             for i in range(2):
-                turt.forward(rect.width)
-                turt.left(90)
-                turt.forward(rect.height)
-                turt.left(90)
-            turt.hideturtle()
+                trt.forward(rect.width)
+                trt.left(90)
+                trt.forward(rect.height)
+                trt.left(90)
+            trt.hideturtle()
 
-        turt.color("#b5e3d8")
+        trt.color("#red")
         for sq in list_squares:
-            turt.showturtle()
-            turt.up()
-            turt.goto(sq.x, sq.y)
-            turt.down()
+            trt.showturtle()
+            trt.up()
+            trt.goto(sq.x, sq.y)
+            trt.down()
             for i in range(2):
-                turt.forward(sq.width)
-                turt.left(90)
-                turt.forward(sq.height)
-                turt.left(90)
-            turt.hideturtle()
+                trt.forward(sq.width)
+                trt.left(90)
+                trt.forward(sq.height)
+                trt.left(90)
+            trt.hideturtle()
 
         turtle.exitonclick()
