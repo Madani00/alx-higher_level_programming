fbcc39be5cf09ef60434529bdcc5a1071f5dfe05
Madani00 <mamchart@gmail.com>
2024-04-08 11:37:38 -0700

    well done

---
+++ 4-concat.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+
+// a script that prints two arguments passed to it, in the following format: “ is ”
+const { argv } = require('node:process');
+console.log(argv[2] + ' is ' + argv[3]);
