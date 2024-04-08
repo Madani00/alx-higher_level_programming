1029caf2ad39ad4037d00386425c6846b24ba06a
Madani00 <mamchart@gmail.com>
2024-04-08 06:51:43 -0700

    well done

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,7 +3,7 @@
 // a script that prints a message depending of the number of arguments passed:
 const { argv } = require('node:process');
 if (argv[2]) {
-	console.log('Argument found');
+  console.log('Argument found');
 } else {
-	console.log('No argument');
+  console.log('No argument');
 }
