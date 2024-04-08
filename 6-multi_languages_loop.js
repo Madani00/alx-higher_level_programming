4468f1c72688c5fbc58c477cfcae643a16c3fb78
El Madani <123333191+Madani00@users.noreply.github.com>
2024-04-08 18:47:31 +0000

    Delete 6-multi_languages_loop.js

--- 6-multi_languages_loop.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-
-// prints 3 lines: but by using an array of string and a loop “JavaScript is amazing”
-const myVars = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let i = 0; i < myVars.length; i++) {
-  console.log(myVars[i]);
-}
