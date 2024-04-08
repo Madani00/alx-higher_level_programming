ef581c6b4347b3b24de5f66cd30ee3a36bd0ca77
Madani00 <mamchart@gmail.com>
2024-04-08 06:22:07 -0700

    well done

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// a script that prints 3 lines
+const firLine = 'C is fun';
+const secLine = 'Python is cool';
+const thirLine = 'JavaScript is amazing';
+
+console.log(firLine);
+console.log(secLine);
+console.log(thirLine);
