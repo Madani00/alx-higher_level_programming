d77c55a9d92ddfb9d8f2f9aa1485cb2c15b0f9ca
Madani00 <mamchart@gmail.com>
2024-04-13 15:08:16 -0700

    well its challenging how BINARY you know, also fix pycodestyle

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,19 +7,20 @@ from the database hbtn_0e_0_usa
 import MySQLdb
 from sys import argv
 
-if __name__ = "__main__":
-    db = MySQLdb.connect(
-        host='localhost',
-        port=3306,
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3]
-    )
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE BINARY name LIKE
-            'N%' ORDER BY id ASC")
-    output = cursor.fetchall()
-    for row in output:
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+
+    cur = db.cursor()
+    query = "SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id"
+    cur.execute(query)
+
+    for row in cur.fetchall():
         print(row)
-    cursor.close()
+
+    cur.close()
     db.close()
