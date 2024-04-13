fe046b847157218cdd0cb948d964cf7962d5a72c
Madani00 <mamchart@gmail.com>
2024-04-13 12:34:49 -0700

    well its challenging how BINARY you know, also fix pycodestyle

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
-a script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
-- i didn't specify the host=localhost and port=3306, cause they are in by default
+- a script that lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa
+- i didn't specify the host=localhost and port=3306, exist by default
 """
 import MySQLdb
 from sys import argv
@@ -13,7 +14,8 @@ if __name__ = "__main__":
         db=argv[3]
     )
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY \
+            'N%' ORDER BY id ASC")
     output = cursor.fetchall()
     for row in output:
         print(row)
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.
+- a script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument.
 - must use 'format' to create the SQL query with the user input
 """
 import MySQLdb
@@ -13,7 +14,8 @@ if __name__ = "__main__":
         db=argv[3]
     )
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY \ '{}' ORDER BY id ASC".format(argv[4]))
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY \
+            '{}' ORDER BY id ASC".format(argv[4]))
     output = cursor.fetchall()
     for row in output:
         print(row)
