383fba1c70daf5725991ddd5bd3b675dbcec438e
Madani00 <mamchart@gmail.com>
2024-04-13 12:15:26 -0700

    well

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,7 +10,7 @@ if __name__ = "__main__":
     db = MySQLdb.connect(
         user=argv[1],
         passwd=argv[2],
-        db=argv[3],
+        db=argv[3]
     )
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id ASC")
---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.
+- must use 'format' to create the SQL query with the user input
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ = "__main__":
+    db = MySQLdb.connect(
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        userinput=argv[4]
+    )
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name='{}' ORDER BY id ASC".format(userinput))
+    output = cursor.fetchall()
+    for row in output:
+        print(row)
+    cursor.close()
+    db.close()
