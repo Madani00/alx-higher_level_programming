940054b9d60eb514c2ddc9a42d588d70c7548903
Madani00 <mamchart@gmail.com>
2024-04-14 11:40:35 -0700

    fix; pycodestyle

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 """our model to be imported
 """
-
 from sqlalchemy import Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
-
 
 Base = declarative_base()
 
 
 class State(Base):
-    __tablename__ = 'states'
+    """class inherits from Base"""
+    __tablename__ = "states"
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
