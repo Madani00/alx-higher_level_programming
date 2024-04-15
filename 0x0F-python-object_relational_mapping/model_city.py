c285da1c66d9100d8a1be150a86cf1fd969b93a1
Madani00 <mamchart@gmail.com>
2024-04-15 05:51:48 -0700

    good

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""our model to be imported
+"""
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from model_state import State
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class City(Base):
+    """class inherits from Base"""
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey=("states.id"), nullable=False)
+    state = relationship("State", foreign_keys="City.state_id")
