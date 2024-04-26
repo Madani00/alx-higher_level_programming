28a7ba461b195079f7b106222367445f1ad38f26
Madani00 <mamchart@gmail.com>
2024-04-26 13:00:03 -0700

    technical interview

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -15,8 +15,8 @@ def find_peak(list_of_integers):
 
     mid = len(list_of_integers) // 2
     if list_of_integers[mid] < list_of_integers[mid-1]:
-        return find_peak(list_of_integers[:mid+1])
+        return find_peak(list_of_integers[:mid])
     elif list_of_integers[mid] < list_of_integers[mid+1]:
-        return find_peak(list_of_integers[mid-1:])
+        return find_peak(list_of_integers[mid+1:])
     else:
         return list_of_integers[mid]
