d24d92470a25627bc378047dad273e593c19ed4d
Madani00 <mamchart@gmail.com>
2024-04-26 12:53:47 -0700

    technical interview

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,19 +4,20 @@
 
 def find_peak(list_of_integers):
     """function"""
-    left, right = 0, len(list_of_integers) - 1
-
     if len(list_of_integers) == 0:
         return None
 
     if len(list_of_integers) == 1:
         return list_of_integers[0]
 
-    while left <= right:
-        mid = left + (right - left) // 2
-        if list_of_integers[mid] >= list_of_integers[mid-1] and list_of_integers[mid] >= list_of_integers[mid+1]: 
-            return list_of_integers[mid]
-        elif list_of_integers[mid] < list_of_integers[mid-1]:
-            left = mid + 1
-        else:
-            right = mid - 1
+    if len(list_of_integers) == 2:
+        return max(list_of_integers)
+
+    mid = len(list_of_integers) // 2
+    if list_of_integers[mid] < list_of_integers[mid-1]:
+        return find_peak(list_of_integers[:mid+1])
+    elif list_of_integers[mid] < list_of_integers[mid+1]:
+        return find_peak(list_of_integers[mid-1:])
+    else:
+        return list_of_integers[mid]
+    
