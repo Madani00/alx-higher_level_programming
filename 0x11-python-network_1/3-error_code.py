9831a52a91705146d0b43a9a18529d0fbb791678
Madani00 <mamchart@gmail.com>
2024-04-27 11:27:48 -0700

    fix : PYCODESTYLE

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """manage urllib.error.HTTPError exceptions and print: Error code:"""
-from urllib.request import urlopen
+import urllib.request
 from sys import argv
 
 
 if __name__ == "__main__":
     try:
-        with urlopen(argv[1]) as resp:
-            d = resp.read()
-            print(d.decode("utf-8")
-    except urllib.error.HTTPError as er:
-        print("Error code: {}".format(er.code))
+        with urllib.request.urlopen(argv[1]) as response:
+            da = response.read()
+            print(da.decode('utf-8'))
+    except urllib.error.HTTPError as err:
+        print("Error code: {}".format(err.code))
