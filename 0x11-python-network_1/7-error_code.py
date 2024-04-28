c3d9833f70d80c97cbb6adad602c4b7f73f69837
Madani00 <mamchart@gmail.com>
2024-04-28 11:07:59 -0700

    if __name__ == "__main__":

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Display the value of X-Request-Id."""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+
+    r = requests.get(argv[1])
+    print(r.code_status)
