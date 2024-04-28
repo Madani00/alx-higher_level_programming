00ea6edd15c13aeb6ffabb8a68ccd6987f8168ea
Madani00 <mamchart@gmail.com>
2024-04-28 10:43:18 -0700

    fix : data=...

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -6,6 +6,6 @@ from sys import argv
 
 if __name__ == "__main__":
 
-    email = {"email": argv[2]}
-    r = requests.post(argv[1], data=email)
+    em = {"email": argv[2]}
+    r = requests.post(argv[1], data=em)
     print(r.text)
