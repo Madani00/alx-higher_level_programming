efc77e49b5245c1e2fa411182c059cf769e34a0b
Madani00 <mamchart@gmail.com>
2024-04-28 11:12:09 -0700

    fix : added else condition

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,4 +7,7 @@ from sys import argv
 if __name__ == "__main__":
 
     r = requests.get(argv[1])
-    print(r.code_status)
+    if (r.code_status >= 400):
+        print(r.code_status)
+    else:
+        print(r.text)
