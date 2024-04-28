bbc8afd1ad6001dd5e57bde73030f3edf9667c6a
Madani00 <mamchart@gmail.com>
2024-04-28 10:16:40 -0700

    well done

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""a script that fetches a website"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+
+    r = requests.get(argv[1])
+    res_id = r.getheader("X-Request-Id")
+    print(res_id)
