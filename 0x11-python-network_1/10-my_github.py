032207e0797525435574cb23df6ed1f2106eb98f
Madani00 <mamchart@gmail.com>
2024-04-28 18:22:22 -0700

    ggooooood

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+a script that takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 2:
+        username = sys.argv[1]
+        token = sys.argv[2]
+
+        response = requests.get('https://api.github.com/user', auth=(username, token))
+
+        if response.status_code == 200:
+            print(response.json()['id'])
+        else:
+            print(None)
