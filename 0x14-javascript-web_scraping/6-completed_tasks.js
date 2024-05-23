42b01902fedff953b729b2667842b7e0cd9314f8
Madani00 <mamchart@gmail.com>
2024-05-22 17:39:43 -0700

    great

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const request = require('request');
+const arg = process.argv;
+
+request(arg[2], (err, res`, body) => {
+  if (!err) {
+    const data = JSON.parse(body);
+
+    const result = {};
+    data.forEach(item => {
+      if (!(item.userId in result) && item.completed) {
+        result[item.userId] = 1;
+      } else if (item.completed) {
+        result[item.userId] += 1;
+      }
+    });
+    console.log(result);
+  }
+});
